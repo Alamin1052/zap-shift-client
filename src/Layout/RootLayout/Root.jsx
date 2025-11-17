91f60a4fd7da494255e1d1acf8ff697abebf4819
Alamin <alaminetc9@gmail.com>
2025-11-17 18:50:56 +0600

    successfully connect to github repu

---
+++ src/Layout/RootLayout/Root.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Root = () => {
+    return (
+        <div>
+            <h1>this Root layouts</h1>
+        </div>
+    );
+};
+
+export default Root;
---
+++ src/Routes/Router.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router";
+import Root from "../Layout/RootLayout/Root";
+
+
+export const router = createBrowserRouter([
+    {
+        path: '/',
+        Component: Root
+    }
+])
